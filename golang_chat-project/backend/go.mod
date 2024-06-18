a411ff62ab1d6060eef106bb1291250a5a676986
buynaa <buynaaesther@gmail.com>
2024-06-18 17:02:58 +0800

    first commit

---
+++ golang_chat-project/backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/buynaa0314/golang_chat
+
+go 1.21.11
+
+require github.com/gorilla/websocket v1.5.3
---
+++ golang_chat-project/backend/main.go
@@ -0,0 +1,39 @@
+package main 
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/buynaa0314/golang_chat-project/pkg/websocket"
+)
+
+func serveWS(pool *websSocket.Pool, w http.ResponseWriter, r *http.Request) {
+    fmt.Println("webSocket endpoint reached")
+
+    conn, err := webSocket.Upgrade(w, r)
+    if err != nil {
+        fmt.Fprintf(w, "%+v\n", err)
+        return
+    }
+
+    client := &webSocket.Client{
+        Conn: conn,
+        Pool: pool,
+    }
+    pool.Register <- client
+    client.Read()
+}
+
+func setupRoutes() {
+    pool := webSocket.NewPool()
+    go pool.Start()
+
+    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+        serveWS(pool, w, r)
+    })
+}
+
+func main() {
+    fmt.Println("Buynaa's full stack chat project")
+    setupRoutes()
+    http.ListenAndServe(":9000", nil)
+}
